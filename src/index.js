b7602882c3e00e9f8aca3ddaaafded59665ba02f
Dmytro Rabenko <rabenkod@ukr.net>
2023-07-30 18:20:12 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import { Provider } from 'react-redux';//for redux working
 import { PersistGate } from 'redux-persist/integration/react';////for redux-persist working
 import store, {persistor} from './store';
-
-
-
 import App from './components/app/app';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
